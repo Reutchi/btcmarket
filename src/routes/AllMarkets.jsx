22f313f07978cf3af39ce44279f79c7727dcae96
ReutchiDan <reutchi1997@icloud.com>
2023-04-11 20:45:13 +0300

    add button load more in section all markets

--- src/routes/AllMarkets.jsx
+++ src/routes/AllMarkets.jsx
@@ -1,4 +1,4 @@
-import {useContext, useEffect} from "react";
+import {useContext, useEffect, useState} from "react";
 import {StatisticContext} from "../context/StatisticContext/StatisticProvider.jsx";
 import ListCoins from "../utils/components/listCoins.jsx";
 import Button from "../utils/components/button.jsx";
@@ -6,15 +6,20 @@ import {RingLoader} from "react-spinners";
 
 const AllMarkets = () => {
 
-
     const {coins,fetchData,isLoading} = useContext(StatisticContext)
 
     useEffect(() => {
         fetchData()
     }, [])
 
+    const [visible,setVisible] = useState(11)
+
+    const showMore = () => {
+        setVisible(prevState => prevState + 5)
+    }
+
     return(
-        <section className=''>
+        <section className='mt-32'>
             <div className='flex justify-center'>
                 {   isLoading
                     ? <div className='flex justify-center'><RingLoader color='#36d7b7' /></div>
@@ -26,19 +31,23 @@ const AllMarkets = () => {
                             <th className='hidden md:table-cell'>24hr Change</th>
                             <th className='hidden md:table-cell'>Market Cap</th>
                         </tr>
-                        {coins.map((coin,index) => {
+                        {coins.slice(0,visible).map((coin,index) => {
                             return(
-                                <ListCoins
-                                    key={index}
-                                    coin={coin}
-                                />
+                                    <ListCoins
+                                        key={index}
+                                        coin={coin}
+                                    />
                             )
                         })}
                         </tbody>
                     </table>
                 }
             </div>
-            <div className='flex justify-center pt-10'>
+            <div className='flex space-x-3 justify-center pt-10'>
+                <Button
+                    version='cursor-pointer bg-lightGreen'
+                    onClick={showMore}
+                    children='Load More'/>
                 <Button
                     href='/'
                     children='Back To Main'
